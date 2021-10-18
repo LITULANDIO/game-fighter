bc38e64935da465803926ec36313514de4da0842
litus <litulandio@gmail.com>
2021-10-18 23:45:19 +0200

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+const configPrettier = fs.readFileSync('./.prettierrc');
+
 module.exports = {
   root: true,
+  ignorePatterns: ['dist/*'],
   env: {
     node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', JSON.parse(configPrettier)]
+  }
+};
